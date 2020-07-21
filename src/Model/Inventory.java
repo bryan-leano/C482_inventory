bb408581ebb25b53f4e9f26e421a83478cb25d7b
Bryan Leanoe <bryan.leano@gmail.com>
2020-07-20 18:45:15 -0600

    filled in Product, Inventory Classes

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -1,7 +1,58 @@
 package Model;
-import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Inventory {
 
+    private static ObservableList<Part> allParts = FXCollections.observableArrayList();
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+
+    public static void addPart(Part part) {
+        allParts.add(part);
+    }
+
+    public static void addProduct(Product product) {
+        allProducts.add(product);
+    }
+
+    public static Part lookupPart (int partId) {
+
+    }
+
+    public static  Product lookupProduct (int productId) {
+
+    }
+
+    public ObservableList<Part> lookupPart(String partName) {
+
+    }
+
+    public ObservableList<Product> lookupProduct(String productName) {
+
+    }
+
+    public static void updatePart(int index, Part selectedPart) {
+
+    }
+
+    public static void updateProduct(int index, Product newProduct) {
+
+    }
+
+    public static boolean deletePart(Part selectedPart) {
+
+    }
+
+    public static boolean deleteProduct(Product selectedProduct) {
+
+    }
+
+    public static ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public static ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
 
 }
