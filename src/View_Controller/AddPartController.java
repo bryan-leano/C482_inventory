4b983a57b872e596188c32a5281833466294faf2
Bryan Leanoe <bryan.leano@gmail.com>
2020-07-22 22:43:41 -0600

    add AddPart fxml

--- src/View_Controller/AddPartController.java
+++ src/View_Controller/AddPartController.java
@@ -1,4 +1,63 @@
 package View_Controller;
 
-public class AddPartController {
+import Model.Inhouse;
+import Model.Outsourced;
+import Model.Part;
+import static Model.Inventory.addPart;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javax.xml.bind.ValidationException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.scene.control.RadioButton;
+
+public class AddPartController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+
+
+    }
+
+    @FXML
+    private TextField partIdTxt;
+
+    @FXML
+    private TextField nameTxt;
+
+    @FXML
+    private TextField invTxt;
+
+    @FXML
+    private TextField priceCostTxt;
+
+    @FXML
+    private TextField maxTxt;
+
+    @FXML
+    private TextField minTxt;
+
+    @FXML
+    private TextField machineIdTxt;
+
+    @FXML
+    private RadioButton InHouseRBtn;
+
+    @FXML
+    private RadioButton OutsourcedRBtn;
+
 }
