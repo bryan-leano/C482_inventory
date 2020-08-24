354cbd565a9f6700bbac9fdb8db1605b96d67bbb
Bryan Leanoe <bryan.leano@gmail.com>
2020-08-24 17:43:34 -0600

    add exception handling

--- src/View_Controller/AddPartController.java
+++ src/View_Controller/AddPartController.java
@@ -35,26 +35,35 @@ public class AddPartController implements Initializable {
     @FXML
     void onActionSavePart(ActionEvent event) throws IOException {
 
-        int id = Integer.parseInt(partIdTxt.getText());
-        String name = nameTxt.getText();
-        double price = Double.parseDouble(priceCostTxt.getText());
-        int stock = Integer.parseInt(invTxt.getText());
-        int max = Integer.parseInt(maxTxt.getText());
-        int min = Integer.parseInt(minTxt.getText());
-        boolean isInHouse;
+        try
+        {
+            int id = Integer.parseInt(partIdTxt.getText());
+            String name = nameTxt.getText();
+            double price = Double.parseDouble(priceCostTxt.getText());
+            int stock = Integer.parseInt(invTxt.getText());
+            int max = Integer.parseInt(maxTxt.getText());
+            int min = Integer.parseInt(minTxt.getText());
+            boolean isInHouse;
 
-        if(InHouseRBtn.isSelected())
-            isInHouse = true;
-        else
-            isInHouse = false;
+            if(InHouseRBtn.isSelected())
+                isInHouse = true;
+            else
+                isInHouse = false;
 
-        int machineId = 0;
-        Inventory.addPart(new InHouse(id, name, price, stock,  min, max, machineId));
+            int machineId = 0;
+            Inventory.addPart(new InHouse(id, name, price, stock,  min, max, machineId));
 
-        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
-        stage.setScene(new Scene(scene));
-        stage.show();
+            stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.println("Please enter valid values in fields.");
+            System.out.println("Exception: " + e);
+            System.out.println("Exception: " + e.getMessage());
+        }
 
     }
 
